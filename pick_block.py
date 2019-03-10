8dc5f527084c89393e17bca8b877475a63273abc
sammyhamwi <sammyhamwi@gmail.com>
2019-03-10 16:06:24 -0500

    first commit: pick_block function.

---
+++ pick_block.py
@@ -0,0 +1,48 @@
+# import the necessary packages
+import numpy as np
+
+def pick_block(tower_state):
+	block_index = [0,0]
+
+	toprows = len(tower_state)-3
+
+	x = 5;
+	for x in range(2, toprows):
+		if (tower_state[x][0]==1 and tower_state[x][1]==1 and tower_state[x][2]==1):
+			block_index[0] = x;
+			block_index[1] = 1;
+			return block_index
+
+
+
+
+
+
+
+
+
+
+
+
+
+tower_state = np.array([[1, 1, 1],
+						[1, 1, 1], 
+					    [1, 1, 1],
+						[0, 1, 1], 
+					    [0, 1, 1],
+						[1, 0, 1], 
+					    [0, 1, 1],
+						[1, 0, 1], 
+					    [1, 0, 1],
+						[1, 0, 1], 
+					    [0, 1, 1],
+						[1, 1, 1], 
+					    [1, 0, 1],
+						[0, 1, 1], 
+					    [1, 0, 1],
+						[0, 1, 1], 
+					    [1, 1, 0],
+						[1, 0, 1]])
+
+block_index = pick_block(tower_state)
+print("\nBlock Index: ["+str(block_index[0])+"]["+str(block_index[1])+"]\n")
