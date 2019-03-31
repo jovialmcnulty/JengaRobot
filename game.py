da9f8ab0fb0756f9d42c2338ec7bb1be65b47558
cloudy <im@joe.cloud>
2019-03-31 16:13:55 -0500

    Added more method stubs

--- game.py
+++ game.py
@@ -4,13 +4,36 @@
 # on it (from the ../cv/ folder), and executing move commands on the UR5
 ################################################################################
 
+# This constant will be used to convert aruco coords into ur5 coords
+GLOBAL_OFFSET = (0,0,0,0)
+CAM_LOCATION = (0,0,0,0)
+class Block:
+    aruco_coords = (0,0,0,0)
+    ur5_coords = (0,0,0,0)
+    id_number = -1
 
 # Takes in the coordinates for a chosen aruco tag and turns it into UR5 coordinates
 def change_coordinate_space(aruco_coords):
+    return aruco_coords += GLOBAL_OFFSET
 
 # Returns an image from ROS
 def get_image():
+    #todo this
 
+def return_to_cam():
+    #return the UR5 to the camera sensing location
+
+def get_game_state():
+    
+def execute_move(block):
+    #todo execute move
+    return_to_cam()
 
 # Starts ROS server if not started, initializes robot and game state
 def main():
+    return_to_cam()
+    #   todo initial setup
+    get_game_state()
+    # todo choose block to pick
+    picked_block = 0
+    execute_move(0)
