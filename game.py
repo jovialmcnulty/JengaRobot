5bf32cde70a87dd1172511862d539bd96df6f907
cloudy <im@joe.cloud>
2019-03-31 15:40:14 -0500

    Added a TODO, Created game script

---
+++ game.py
@@ -0,0 +1,16 @@
+################################################################################
+# This script will handle starting ROS if it is not started yet,
+# pulling the images from the camera in order to run CV
+# on it (from the ../cv/ folder), and executing move commands on the UR5
+################################################################################
+
+
+# Takes in the coordinates for a chosen aruco tag and turns it into UR5 coordinates
+def change_coordinate_space(aruco_coords):
+
+# Returns an image from ROS
+def get_image():
+
+
+# Starts ROS server if not started, initializes robot and game state
+def main():
---
+++ todo.md
@@ -0,0 +1,10 @@
+# General
+- [ ] Create poster for Innovation Day 4/8
+- [ ] Update website
+- [ ] Sprint burndown for 4/8
+- [ ] Demo 4/8
+
+# ROS
+- [ ] Find a way to run ROS commands through the game engine game.py script
+- [ ] Get images from ROS through the game.py script get_image()
+- [ ] Create offset matrix for finding ROS coordinates for ARUKO tags
